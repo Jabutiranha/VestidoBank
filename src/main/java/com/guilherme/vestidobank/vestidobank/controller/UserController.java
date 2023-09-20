c28abfd4d10e474d4d58e61f83686422b0e9903e
Guilherme De Souza <souzadegui@gmail.com>
2023-09-19 21:51:41 -0300

    token ok

--- src/main/java/com/guilherme/vestidobank/vestidobank/controller/UserController.java
+++ src/main/java/com/guilherme/vestidobank/vestidobank/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.guilherme.vestidobank.vestidobank.controller;
 
+import com.guilherme.vestidobank.vestidobank.exception.ResourceNotFoundException;
 import com.guilherme.vestidobank.vestidobank.model.User;
 import com.guilherme.vestidobank.vestidobank.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -21,8 +25,45 @@ public class UserController {
         return repository.findAll();
     }
 
-    @PostMapping("/criar")
+    @GetMapping("/usuarios/{id}")
+    public ResponseEntity<User> getUserByID(@PathVariable(value = "id") Long userId)
+            throws ResourceNotFoundException {
+        User user = repository.findById(userId).orElseThrow(()->
+                new ResourceNotFoundException("Usuario nao encontrado: " + userId));
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/usuarios")
     public User createUser(@Validated @RequestBody User user){
         return repository.save(user);
+    }
+
+    @PutMapping("/usuarios/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long userId,
+                                           @Validated @RequestBody User detalhes)
+            throws ResourceNotFoundException{
+        User user = repository.findById(userId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Usuario nao encontrado: " + userId));
+        user.setEmail(detalhes.getEmail());
+        user.setSobrenome(detalhes.getSobrenome());
+        user.setNome(detalhes.getNome());
+        user.setSenha(detalhes.getSenha());
+        user.setSaldo(detalhes.getSaldo());
+        final User updatedUser = repository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    public Map<String, Boolean> deleteUser(
+            @PathVariable(value = "id") Long userId
+    ) throws Exception{
+        User user = repository.findById(userId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Usuario nao encontrado: " + userId));
+        repository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
